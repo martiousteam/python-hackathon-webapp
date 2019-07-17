342f25a9561b055564d56909edae79109e00ad57
Pratik Ajmera <17395547+pratikajmera@users.noreply.github.com>
2019-07-16 21:46:14 -0400

    Updates to initialize database

--- initializedb.py
+++ initializedb.py
@@ -1,5 +1,5 @@
 import sys
-sys.path.insert(0,"/var/www/python/python-hackathon-webapp/")
+# sys.path.insert(0,"/var/www/python/python-hackathon-webapp/")
 
 from hackathon import db
 from hackathon import app
@@ -14,48 +14,67 @@ if __name__ == "__main__":
         db.create_all()
 
     # TBD - Change code to make sure record is inserted only if it does not exist.
-    user1 = User(id=1,username='TestUser',email='martious@gmail.com',name='Test User',password='test',is_kerberos=False)
-    user2 = User(id=2,username='himanshu',email='himanshu.shrotri@gmail.com',name='Himanshu Shrotri',password='test',is_kerberos=False)
-    user3 = User(id=3,username='pratik',email='pratikajmera@gmail.com',name='Pratik Ajmera',password='test',is_kerberos=False)
-    user4 = User(id=4,username='ajmera@INTRANET.MARTIOUS.COM',email='pratik.ajmera@martious.com',name='Pratik Ajmera',is_kerberos=True)
-    db.session.add_all([user1,user2,user3,user4])
+    users=[]
+    users.append(User(id=1,username='TestUser',email='martious@gmail.com',name='Test User',password='test',is_kerberos=False))
+    users.append(User(id=2,username='himanshu',email='himanshu.shrotri@gmail.com',name='Himanshu Shrotri',password='test',is_kerberos=False))
+    users.append(User(id=3,username='pratik',email='pratikajmera@gmail.com',name='Pratik Ajmera',password='test',is_kerberos=False))
+    users.append(User(id=4,username='ajmera@INTRANET.MARTIOUS.COM',email='pratik.ajmera@martious.com',name='Pratik Ajmera',is_kerberos=True))
+    db.session.add_all(users)
 
-    participant1 = Participant(id=1,email='martious@gmail.com',name='Test User',user=user1,team_name='Martious 1')
-    participant2 = Participant(id=2,email='himanshu.shrotri@gmail.com',name='Himanshu Shrotri',user=user2,team_name='Martious 2')
-    participant3 = Participant(id=3,email='pratikajmera@gmail.com',name='Pratik Ajmera',user=user3,team_name='Martious 1')
-    participant4 = Participant(id=4,email='pratik.ajmera@martious.com',name='Pratik Ajmera',user=user4,team_name='Martious 2')
-    db.session.add_all([participant1,participant2,participant3,participant4])
+    participants=[]
+    participants.append(Participant(id=1,email='martious@gmail.com',name='Test User',user_id=1,team_name='Martious 1'))
+    participants.append(Participant(id=2,email='himanshu.shrotri@gmail.com',name='Himanshu Shrotri',user_id=2,team_name='Martious 2'))
+    participants.append(Participant(id=3,email='pratikajmera@gmail.com',name='Pratik Ajmera',user_id=3,team_name='Martious 1'))
+    participants.append(Participant(id=4,email='pratik.ajmera@martious.com',name='Pratik Ajmera',user_id=4,team_name='Martious 2'))
+    db.session.add_all(participants)
 
-    competition1 = Competition(id=1,name='Martious Hackathon 1', description="Join in the world's most excieting Coding Competition @ Martious.com 1")
-    competition2 = Competition(id=2,name='Martious Hackathon 2', description="Join in the world's most excieting Coding Competition @ Martious.com 2")
+    competitions=[]
+    competitions.append(Competition(id=1,name='Hackathon 1', description="Join in the world's most excieting Coding Competition @ Martious.com 1"))
+    competitions.append(Competition(id=2,name='Hackathon 2', description="Join in the world's most excieting Coding Competition @ Martious.com 2"))
 
-    competition1.participants.append(participant1)
-    competition1.participants.append(participant2)
-    competition1.participants.append(participant3)
-    competition1.participants.append(participant4)
-    competition2.participants.append(participant1)
+    # First pattern to insert data in relationship
+    competitions[0].participants.append(participants[0])
+    competitions[0].participants.append(participants[1])
+    
+    db.session.add_all(competitions)
+    
+    # Another pattern to insert data in relationship
+    competitionparticipants=[]
+    competitionparticipants.append(CompetitionParticipant(participant_id=3,competition_id=1))
+    competitionparticipants.append(CompetitionParticipant(participant_id=4,competition_id=1))
+    competitionparticipants.append(CompetitionParticipant(participant_id=1,competition_id=2))
 
-    db.session.add_all([competition1,competition2])
+    db.session.add_all(competitionparticipants)
 
-    problemstatement1 = ProblemStatement(competition_id=1, problem_summary="Problem Summary 1.1",
-                                         problem_text="Sample Problem Text Goes Here 1", problem_answer="hello")
-    problemstatement2 = ProblemStatement(competition_id=1, problem_summary="Problem Summary 1.2",
-                                         problem_text="Sample Problem Text Goes Here 2", problem_answer="100")
-    problemstatement3 = ProblemStatement(competition_id=2, problem_summary="Problem Summary 2.1",
-                                         problem_text="Sample Problem Text Goes Here 3", problem_answer="hello")
-    problemstatement4 = ProblemStatement(competition_id=2, problem_summary="Problem Summary 2.2",
-                                         problem_text="Sample Problem Text Goes Here 4", problem_answer="1220")
-    problemstatement5 = ProblemStatement(competition_id=2, problem_summary="Problem Summary 2.3",
-                                         problem_text="Sample Problem Text Goes Here 5", problem_answer="123")
-    problemstatement6 = ProblemStatement(competition_id=2, problem_summary="Problem Summary 2.4",
-                                         problem_text="Sample Problem Text Goes Here 6", problem_answer="115")
+    problemstatements=[]
+        
+    problemstatements.append(ProblemStatement(competition_id=1, problem_summary="Problem Summary 1.1",
+                                         problem_text="Sample Problem Text Goes Here 1", problem_answer="hello"))
+                                         
+    problemstatements.append(ProblemStatement(competition_id=1, problem_summary="Problem Summary 1.2",
+                                         problem_text="Sample Problem Text Goes Here 2", problem_answer="100"))
 
-    problemstatement1.participants.append(participant1)
-    problemstatement2.participants.append(participant2)
-    problemstatement3.participants.append(participant3)
-    problemstatement1.participants.append(participant4)
+    problemstatements.append(ProblemStatement(competition_id=2, problem_summary="Problem Summary 2.1",
+                                         problem_text="Sample Problem Text Goes Here 3", problem_answer="hello"))
 
-    db.session.add_all([problemstatement1, problemstatement2, problemstatement3, problemstatement4,
-                        problemstatement5, problemstatement6])
+    problemstatements.append(ProblemStatement(competition_id=2, problem_summary="Problem Summary 2.2",
+                                         problem_text="Sample Problem Text Goes Here 4", problem_answer="1220"))
+
+    problemstatements.append(ProblemStatement(competition_id=2, problem_summary="Problem Summary 2.3",
+                                         problem_text="Sample Problem Text Goes Here 5", problem_answer="123"))
+
+    problemstatements.append(ProblemStatement(competition_id=2, problem_summary="Problem Summary 2.4",
+                                         problem_text="Sample Problem Text Goes Here 6", problem_answer="115"))
+
+    db.session.add_all(problemstatements)
+    
+    participantproblemstatements=[]
+
+    participantproblemstatements.append(ParticipantProblemStatement(participant_id=1,problemstatement_id=1))
+    participantproblemstatements.append(ParticipantProblemStatement(participant_id=2,problemstatement_id=2))
+    participantproblemstatements.append(ParticipantProblemStatement(participant_id=3,problemstatement_id=3))
+    participantproblemstatements.append(ParticipantProblemStatement(participant_id=4,problemstatement_id=1))
+
+    db.session.add_all(participantproblemstatements)
 
     db.session.commit()
